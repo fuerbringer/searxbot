25ed6330194bd8fdfff00371290bd0b9346fd3fe
fuerbringer <severin@protonmail.ch>
2017-03-12 16:40:42 +0100

    Added option to pass command line args instead of using the config file

--- index.js
+++ index.js
@@ -6,22 +6,44 @@ var commands = require('./libs/commands.js');
 // Default Searx instance
 default_instance = 'https://searx.ch';
 
+function getSearxArgs(obj) {
+	if(obj) {
+		if(obj.token.length &&
+			obj.db_host.length &&
+			obj.db_user.length &&
+			obj.db_pass.length &&
+			obj.db_name.length) {
+		}
+	} else {
+		// parse from command line args directly
+		var argv = require('minimist')(process.argv.slice(2));
+		obj = {
+			token: argv.token,
+			db_host: argv.db_host,
+			db_user: argv.db_user,
+			db_pass: argv.db_pass,
+			db_name: argv.db_name
+		};
+	}
+	return obj;
+}
+
 /**
  * Read DB info and bot token from authentication file
  */
 jsonfile.readFile('auth.json', function(err, obj) {
-	// Telegram bot token
-	var token = obj.token;
+	var args = getSearxArgs(obj);
+
 	// establish MySQL connection
 	connection = mysql.createPool({
 		connectionLimit: 25,
-		host: obj.db_host,
-		user: obj.db_user,
-		password: obj.db_pass,
-		database: obj.db_name});
+		host: args.db_host,
+		user: args.db_user,
+		password: args.db_pass,
+		database: args.db_name});
 
 	// Setup polling way
-	bot = new telegramBot(token, {polling: true});
+	bot = new telegramBot(args.token, {polling: true});
 
 	/**
 	 * Image search with up to 5 results
