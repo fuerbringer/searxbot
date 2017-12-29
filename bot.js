3bd55ae249055568a3af149c7fa403111997d940
fuerbringer <severin@protonmail.ch>
2017-12-29 15:47:28 +0100

    Lay out basic framework with claudiajs.

--- bot.js
+++ bot.js
@@ -1,65 +1,8 @@
-var telegramBot = require('node-telegram-bot-api');
-var sqlite3 = require('sqlite3');
-var jsonfile = require('jsonfile');
-var commands = require('./libs/commands.js');
+const botBuilder = require('claudia-bot-builder')
 
-// Default Searx instance
-default_instance = 'https://searx.ch';
+// Searx instance hosted by the non-profit association La Quadrature du Net
+const defaultInstance = 'https://searx.laquadrature.net/'
 
-function getSearxArgs(obj) {
-	if(obj) {
-		if(obj.token.length &&
-			obj.db_name.length) {
-		}
-	} else {
-		// parse from command line args directly
-		var argv = require('minimist')(process.argv.slice(2));
-		obj = {
-			token: argv.token,
-			db_name: argv.db_name
-		};
-	}
-	return obj;
-}
-
-/**
- * Read DB info and bot token from authentication file
- */
-jsonfile.readFile('auth.json', function(err, obj) {
-	var args = getSearxArgs(obj);
-
-	// establish SQLite connection
-	db = new sqlite3.Database(args.db_name);
-
-	// Setup polling way
-	bot = new telegramBot(args.token, {polling: true});
-
-	/**
-	 * Image search with up to 5 results
-	 */
-	bot.onText(/^\/searximg "(.+)"$/, commands.searximg);
-	bot.onText(/^\/searximg '(.+)'$/, commands.searximg);
-
-	/**
-	 * Perform search with custom amount of results
-	 */
-	bot.onText(/^\/searx "(.+)" (.+)$/, commands.searx);
-	bot.onText(/^\/searx '(.+)' (.+)$/, commands.searx);
-
-	/**
-	 * Short search with up to 5 results
-	 */
-	bot.onText(/^\/searx "(.+)"$/, commands.searx);
-	bot.onText(/^\/searx '(.+)'$/, commands.searx);
-
-	/**
-	 * Set instance (where searches are fetched from)
-	 */
-	bot.onText(/^\/setinstance (.+)/, commands.setInstance);
-
-	/**
-	 * Help output
-	 */
-	bot.onText(/^\/help/, commands.help);
-
-});
+module.exports = botBuilder((request) => {
+  return `Request received for: ${request.text}`
+})
