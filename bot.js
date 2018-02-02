0af700f5aee26a831cca2f372ec541dd1e06d28c
fuerbringer <severin@protonmail.ch>
2018-02-02 20:24:36 +0100

    Make POST request as form and add complaining handle just in case.

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const search = term => {
     method: 'POST',
     uri: defaultInstance,
     json: true,
-    body: {
+    form: {
       q: term,
       format: 'json'
     }
@@ -26,16 +26,16 @@ const search = term => {
           return `${index+1}. [${results.title}](${results.pretty_url})`
         }
         const requestResults = jsonResults.results.slice(0, maxResults)
-        const titleStr = `Here's ${count} result${count > 1 ? "s" : ""} related to *${params.body.q}*.:\n`
+        const titleStr = `Here's ${count} result${count > 1 ? "s" : ""} related to *${params.form.q}*.:\n`
         const resultStr = requestResults.map(buildResults).join(`\n`) // Combine results
         const messageString = titleStr + resultStr
         return new telegramTemplate.Text(messageString).get()
       } else {
-        return new telegramTemplate.Text(`*Sorry!* I couldn't find anything related to *${params.body.q}*.`).get()
+        return new telegramTemplate.Text(`*Sorry!* I couldn't find anything related to *${params.form.q}*.`).get()
       }
     })
     .catch(error => {
-      return new telegramTemplate.Text(`*Sorry!* Something went wrong on our end. Will be back soon.`).get()
+      return new telegramTemplate.Text(`*Sorry!* Something went wrong on our end. Will be back soon. Complain @sevfbr in the meantime.`).get()
     }) 
 }
 
